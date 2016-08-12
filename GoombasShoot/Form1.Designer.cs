d14b2682823f8cfb01572590f56c04499597bf66
Christian Suncin <SunciNx10@gmail.com>
2016-08-12 16:29:18 -0600

    All Project
    Add all project

---
+++ GoombasShoot/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace GoombasShoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imgGoomba = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgGoomba)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgGoomba
+            // 
+            this.imgGoomba.BackColor = System.Drawing.Color.Transparent;
+            this.imgGoomba.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("imgGoomba.BackgroundImage")));
+            this.imgGoomba.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.imgGoomba.Location = new System.Drawing.Point(12, 12);
+            this.imgGoomba.Name = "imgGoomba";
+            this.imgGoomba.Size = new System.Drawing.Size(50, 50);
+            this.imgGoomba.TabIndex = 0;
+            this.imgGoomba.TabStop = false;
+            this.imgGoomba.Click += new System.EventHandler(this.imgGoomba_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblScore.Location = new System.Drawing.Point(12, 432);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(71, 20);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score 0";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTime.Location = new System.Drawing.Point(630, 432);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(77, 20);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "Time: 60";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(710, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(311, 426);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.imgGoomba);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgGoomba)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgGoomba;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GoombasShoot/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoombasShoot
+{
+    public partial class Form1 : Form
+    {
+        int time = 60, point = 0;
+        Boolean A = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(time == 0) //Verificamos que el timer haya llegado o no a 0
+            {
+                timer1.Stop(); //Lo detenemos
+                timer2.Stop();//Detenemos el Goomba
+                MessageBox.Show("Score: "+ point); //Mostramos la puntuación alcanzada
+                point = 0;//Reiniciamos las variables
+                time = 60;
+                lblScore.Text = "Score "+ point;//También el label
+                timer1.Start(); //Comenzamos a disminuir el tiempo desde 60
+                timer2.Start();
+            }
+            else{
+                time = time - 1; //Sino entonces restamos el tiempo de 1 en 1
+                lblTime.Text = "Time: " + time; //Y mostramos el tiempo en todo momento
+            }
+        }
+
+        private void imgGoomba_Click(object sender, EventArgs e)
+        {
+         if(A == false)
+            {
+                point = point + 1;
+                lblScore.Text = "Score " + point;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            moveGoomba();
+        }
+
+        private void moveGoomba() //Con esto haremos que Goomba recorra la pantalla
+        {
+            Random position = new Random();
+            imgGoomba.Location = new Point(position.Next(700),position.Next(382));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (A == false)
+            {
+                timer1.Stop(); //Lo detenemos
+                timer2.Stop();//Detenemos el Goomba
+                button2.Text = "Restart";
+                A = true;
+            }else{
+                timer1.Start();
+                timer2.Start();
+                button2.Text = "Stop";
+                A = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
